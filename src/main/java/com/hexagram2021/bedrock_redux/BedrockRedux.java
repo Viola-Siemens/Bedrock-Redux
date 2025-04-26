71a681901449592de364d6396119448468215228
刘冬煜 <10185101162@stu.ecnu.edu.cn>
2025-04-26 23:09:21 +0800

    Initial Commit

---
+++ src/main/java/com/hexagram2021/bedrock_redux/BedrockRedux.java
@@ -0,0 +1,11 @@
+package com.hexagram2021.bedrock_redux;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(BedrockRedux.MODID)
+public class BedrockRedux {
+	public static final String MODID = "bedrock_redux";
+
+	public BedrockRedux() {
+	}
+}
