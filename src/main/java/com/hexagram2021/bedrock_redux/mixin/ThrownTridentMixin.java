9582c605449930209407653ae1610bf1c5523a84
刘冬煜 <10185101162@stu.ecnu.edu.cn>
2025-05-01 22:25:52 +0800

    三叉戟伤害受主人攻击伤害属性影响

--- src/main/java/com/hexagram2021/bedrock_redux/mixin/ThrownTridentMixin.java
+++ src/main/java/com/hexagram2021/bedrock_redux/mixin/ThrownTridentMixin.java
@@ -1,14 +1,24 @@
 package com.hexagram2021.bedrock_redux.mixin;
 
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.projectile.ThrownTrident;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
 
 @Mixin(ThrownTrident.class)
 public class ThrownTridentMixin {
 	@ModifyExpressionValue(method = "findHitEntity", at = @At(value = "FIELD", target = "Lnet/minecraft/world/entity/projectile/ThrownTrident;dealtDamage:Z"))
 	private boolean bedrock_redux$alwaysTryHitEntity(boolean original) {
 		return false;
+	}
+
+	@ModifyConstant(method = "onHitEntity", constant = @Constant(floatValue = 8.0F))
+	private float bedrock_redux$addAttributeValue(float constant) {
+		ThrownTrident current = (ThrownTrident)(Object)this;
+		return constant + (current.getOwner() instanceof LivingEntity livingEntity ? (float)livingEntity.getAttributeValue(Attributes.ATTACK_DAMAGE) - 1.0F : 0.0F);
 	}
 }
